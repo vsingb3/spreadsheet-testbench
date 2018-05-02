81cf3db60bfb6ca74fd0817ac936b33bf22460cd
Jasdeep Singh <jasdeep.singh@comprotechnologies.com>
2018-05-02 15:50:34 +0530

    adding basic auth

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 //Install express server
 const express = require('express');
+var basicAuth = require('express-basic-auth');
 const path = require('path');
 
 const app = express();
+
+app.use(basicAuth({
+    users: { 'compro': 'c0mpr0' },
+    challenge: true,
+    unauthorizedResponse: getUnauthorizedResponse,
+    realm: 'Leo Credential'
+}))
+
+function getUnauthorizedResponse(req) {
+    return req.auth ?
+        ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected') :
+        'No credentials provided'
+}
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/heroku.html'));
 });
 
